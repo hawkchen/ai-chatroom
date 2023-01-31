6f8bee9943cf4ab34882475560d1f80df4d062e4
hawkchen <hawkchen@potix.com>
2023-01-31 16:08:25 +0800

    * add messages in various ways * decorate messages

--- src/main/java/org/zkoss/support/ChatRoomComposer.java
+++ src/main/java/org/zkoss/support/ChatRoomComposer.java
@@ -1,21 +1,39 @@
 package org.zkoss.support;
 
+import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.*;
 import org.zkoss.zul.*;
 
 public class ChatRoomComposer extends SelectorComposer {
     @Wire(".messageBox")
-    private Vlayout msgBox;
+    private Div msgBox;
     @Wire
     private Textbox myMessage;
 
     private ChatService chatService = new ChatService();
 
-    @Listen("onOK = textbox")
+    @Listen("onOK = textbox ; onClick = #send")
     public void submit(){
-        msgBox.appendChild(new Label(chatService.prompt(myMessage.getValue())));
+        appendMyMessage();
+        appendAiMessage();
         myMessage.setValue("");
+    }
+
+    private void appendAiMessage() {
+        Div msgDiv = new Div();
+        Label aiName = new Label("ChatAI: ");
+        aiName.setSclass("ai");
+        msgDiv.appendChild(aiName);
+        msgDiv.appendChild(new Label(chatService.prompt(myMessage.getValue())));
+        msgBox.appendChild(msgDiv);
+    }
+
+    private void appendMyMessage(){
+        Div msgDiv = new Div();
+        Executions.createComponentsDirectly("<label value=\"Me: \" sclass=\"me\"/>", "zul", msgDiv, null);
+        msgDiv.appendChild(new Label(myMessage.getValue()));
+        msgBox.appendChild(msgDiv);
     }
 
 
